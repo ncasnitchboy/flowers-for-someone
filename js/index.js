9820651c34d76c488bfccee90a2d7ccda0bb52fe
Nguyễn Châu Anh <86773711+ncasnitchboy@users.noreply.github.com>
2024-12-10 16:18:21 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Dear Thuy,...I have something for you'.split('')
+const text = 'Dear Bich Thuy, I have something for you...'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
